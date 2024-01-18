51f9627715996f74cc7927c042501ef741205f58
Tania M <tania.mathern@gmail.com>
2024-01-17 22:58:00 -0800

    Hellow world - welcome route

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-# Put the code for your API here.
+import uvicorn
+from fastapi import FastAPI
 
-# TODO 2: API
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    message = "Welcome to a data science pipeline server"
+    return {"message": message}
+
+
+if __name__ == "__main__":
+    host_name = "0.0.0.0"
+    port_number = 8080
+    uvicorn.run("main:app",
+                host=host_name,
+                port=port_number,
+                log_level="info",
+                reload=True)
