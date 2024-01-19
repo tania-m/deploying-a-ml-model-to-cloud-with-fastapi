9f50c63dc868a90f1fb97340ded84c3f6c7e9104
Tania M <tania.mathern@gmail.com>
2024-01-18 19:27:20 -0800

    WIP

--- fastapi_client_script.py
+++ fastapi_client_script.py
@@ -19,8 +19,9 @@ def make_request(target_url, data_from_json):
 
     request_results = requests.post(target_url, json=data_from_json)
     status_code = request_results.status_code
-    prediction_result = request_results.json()["predictions"]
     print(f"Request status code: {status_code}")
+    print(request_results.json())
+    prediction_result = request_results.json()["predictions"]
     print(f"Prediction result: {prediction_result}")
 
     return status_code, request_results
@@ -28,10 +29,13 @@ def make_request(target_url, data_from_json):
 
 if __name__ == "__main__":
     # For local testing
-    target_url = "http://localhost:8000/predict"
+    target_url = "http://localhost:10000/predict"
+    # target_url = "https://tam-udacity-deploy-model-to-cloud.onrender.com/predict"
     
-    test_data_source_file = "tests/test-request-2.json"
+    test_data_source_file = "tests/test-request.json"
     with open(test_data_source_file) as json_file:
         data_from_json = json.load(json_file)
 
+    print("data_from_json")
+    print(data_from_json)
     make_request(target_url, data_from_json)
